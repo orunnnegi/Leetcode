ac610cd82dd6a87e178c28b8006e53fe22354d26
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-08-24 19:24:36 +0530

    Time: 26 ms (65.22%), Space: 25 MB (51.45%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+    int n= nums.size();
+    if(n==1) return;
+    if(k % n==0) return;
+    int t= n-(k%n);
+    if(k>n){
+        reverse(nums.begin(), nums.begin()+t);
+        reverse(nums.begin()+t, nums.end());
+        reverse(nums.begin(), nums.end());
+    }
+    else{
+        reverse(nums.begin(), nums.begin()+(n-k));
+        reverse(nums.begin()+(n-k), nums.end());
+        reverse(nums.begin(), nums.end());
+
+
+    }
+
+    }
+};
