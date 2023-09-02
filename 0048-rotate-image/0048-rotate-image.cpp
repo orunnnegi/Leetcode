be4c1025aad565d294f3b631f82327659bebab56
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-02 11:44:40 +0530

    Time: 0 ms (100%), Space: 7.1 MB (54.41%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.cpp
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     void transpose(vector<vector<int>> &v, int n, int m){
         for(int i= 0; i<n; ++i){
-            for(int j= i+1; j<m; ++j){
+            for(int j= i+1; j<n; ++j){
                 swap(v[i][j], v[j][i]);
             }
         }
