d40b2998d01e16fc2839a0cc6f1fd025a4e6abfb
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-10 17:43:11 +0530

    Time: 2 ms (56.67%), Space: 5.9 MB (96.89%) - LeetHub

--- 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
+++ 1461-count-all-valid-pickup-and-delivery-options/1461-count-all-valid-pickup-and-delivery-options.cpp
@@ -3,9 +3,18 @@ public:
     const int mod= 1e9+7;
     int countOrders(int n) {
         long long res= 1;
-        for(int i= n*2; i>1; i-=2){
-            int numberOfPairs= ((i*(i-1))%mod)/2;
-            res= (1ll*res*numberOfPairs)%mod;
+        // for(int i= n*2; i>1; i-=2){
+        //     int numberOfPairs= ((i*(i-1))%mod)/2;
+        //     res= (1ll*res*numberOfPairs)%mod;
+        // }
+        // return res;
+
+
+        for(int i= 2; i<= n; ++i){
+            int spaces= (i-1)*2+1;
+            int possible=( spaces*(spaces+1))/2;
+            res*= possible;
+            res%= mod;
         }
         return res;
     }
