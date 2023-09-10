eee6d8e7eaa1070f6dd37dfc095fbcac263fd83e
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-10 09:55:22 +0530

    Time: 1020 ms (11.67%), Space: 51.4 MB (6.86%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    bool jump(int i, int n, vector<int> &nums, vector<int> &dp){
+        if(i>=n-1) return true;
+        if(i>=n) return false;
+        if(dp[i]!=  -1) return dp[i];
+        else{
+            if(nums[i]==0) return dp[i]= 0;
+            else{
+                for(int j= 1; j<=nums[i]; ++j)
+                    if(jump(j+i, n, nums, dp)) return dp[i]= 1;
+            }
+        }
+        return dp[i]= 0;
+    }
+
+    bool canJump(vector<int>& nums) {
+        int n= nums.size();
+        vector<int> dp(n, -1);
+        return jump(0, n, nums, dp);
+    }
+};
