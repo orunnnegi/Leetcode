ac0300d05f21f2e9ff27d48c2fda7c792affd133
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-14 19:16:24 +0530

    Time: 5 ms (15.29%), Space: 6.4 MB (17.74%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.cpp
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     bool isPowerOfTwo(int n) {
-        if(n==INT_MIN) return false;
-        string str= bitset<32>(n).to_string();
-        int count= 0;
-        for(int i= 0; i<32; i++){
-            if(str[i]=='1') count++;
-        }
-        if(count==1) return true;
+        if(n==INT_MIN || n==0) return false;
+        // string str= bitset<32>(n).to_string();
+        // int count= 0;
+        // for(int i= 0; i<32; i++){
+        //     if(str[i]=='1') count++;
+        // }
+        // if(count==1) return true;
+        // return false;
+        if(ceil(log2(n))==floor(log2(n))) return true;
         return false;
     }
 };
