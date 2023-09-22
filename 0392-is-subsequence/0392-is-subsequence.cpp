d6470fa72f29be04f2745322e69320dc3bbc07db
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-22 19:36:21 +0530

    Time: 0 ms (100%), Space: 6.9 MB (10.03%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int textsize= t.length(), patternsize= s.length();
+        if(patternsize==0 && textsize!=0) return true;
+        if(patternsize==0 && textsize==0) return true;
+        if(textsize==0 || patternsize==0) return false;
+        // cout<<textsize<<" "<<patternsize;
+        
+        // if(textsize==0) return false;
+        
+        vector<bool> check(patternsize, false);
+        int k= 0;
+        for(int i= 0; i<textsize; i++){
+            if(t[i]==s[k]){
+                // cout<<
+                check[k]=true;
+                k++;
+            }
+        }
+        if(!check[patternsize-1]) return false;
+        return true;
+    }
+};
