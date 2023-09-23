a7691172116122753f365e9818a646a054c0aca2
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-09-23 19:30:22 +0530

    Time: 49 ms (98.44%), Space: 17 MB (51.45%) - LeetHub

--- 1129-longest-string-chain/1129-longest-string-chain.cpp
+++ 1129-longest-string-chain/1129-longest-string-chain.cpp
@@ -1,22 +1,28 @@
-class Solution {
-public:
-    int longestStrChain(std::vector<std::string>& words) {
-        std::sort(words.begin(), words.end(), [](const std::string& a, const std::string& b) {
-            return a.size() < b.size();
-        });
-        
-        std::unordered_map<std::string, int> dp;
-        int max_chain = 0;
-        for (const auto& word : words) {
-            dp[word] = 1;
-            for (int i = 0; i < word.size(); ++i) {
-                std::string prev_word = word.substr(0, i) + word.substr(i + 1);
-                if (dp.find(prev_word) != dp.end()) {
-                    dp[word] = std::max(dp[word], dp[prev_word] + 1);
+class Solution
+{
+    public:
+        int longestStrChain(std::vector<std::string > &words)
+        {
+            sort(words.begin(), words.end(), [](const string &a, const string &b)
+            {
+                return a.size() < b.size();
+	});
+
+            unordered_map<string, int> dp;
+            int max_chain = 0;
+            for (const auto &word: words)
+            {
+                dp[word] = 1;
+                for (int i = 0; i < word.size(); ++i)
+                {
+                    string prev_word = word.substr(0, i) + word.substr(i + 1);
+                    if (dp.find(prev_word) != dp.end())
+                    {
+                        dp[word] = max(dp[word], dp[prev_word] + 1);
+                    }
                 }
+                max_chain = max(max_chain, dp[word]);
             }
-            max_chain = std::max(max_chain, dp[word]);
+            return max_chain;
         }
-        return max_chain;
-    }
 };
