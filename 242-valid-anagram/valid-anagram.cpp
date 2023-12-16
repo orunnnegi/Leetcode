f7c3d3b4f839e6734cfd720f0eef08a790136b6d
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2023-12-16 08:38:01 +0530

    Time: 8 ms (62.96%) | Memory: 7.8 MB (45.96%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        map<char, int> mpp1;
-        map<char, int> mpp2;
-        for(auto it: s) mpp1[it]++;
-        for(auto it: t) mpp2[it]++;
-        return mpp1==mpp2;
+        // map<char, int> mpp1;
+        // map<char, int> mpp2;
+        // for(auto it: s) mpp1[it]++;
+        // for(auto it: t) mpp2[it]++;
+        // return mpp1==mpp2;
+
+        vector<int> a1(26, 0), a2(26, 0);
+        for(char it: s){int t= it-97; a1[t]++;}
+        for(char it: t){int t= it-97; a2[t]++;}
+        // for(auto it: a1) cout<<it<<" ";
+        return a1==a2;
     }
 };
