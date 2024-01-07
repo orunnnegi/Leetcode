8d27b355506fa1dd8e9f9f405fbeb2dab197aa3d
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-01-07 10:53:42 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (99.35%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -11,34 +11,58 @@ public:
         // }
         // return;
         
+    //     int n= nums.size();
+    //     int a= 0;
+    //     int b= 0;
+    //     int c= 0;
+    //     for(int i = 0; i<n; i++){
+    //         if(nums[i] == 0){
+    //             ++a;
+    //         }
+    //         else if(nums[i] == 1){
+    //             ++b;
+    //         }
+    //         else{
+    //             ++c;    tc= O(N) + O(N) -> O(N) sc= O(1)
+    //         }
+    //     }
+    //     for(int i= 0; i<n; i++){
+    //         if(i<a){
+    //             nums[i] = 0;
+    //             continue;
+    //         }
+    //         else if(i >= a and i< (a+b)){
+    //             nums[i] = 1;
+    //         }
+    //         else{
+    //             nums[i] = 2;
+    //         }
+    //     }
+    //     return ;
+
+    // }
+
         int n= nums.size();
-        int a= 0;
-        int b= 0;
-        int c= 0;
-        for(int i = 0; i<n; i++){
-            if(nums[i] == 0){
-                ++a;
+        int low= 0;
+        int mid= 0;
+        int high= n-1;
+        while(mid<= high)
+        {
+            if(nums[mid] == 0){
+                swap(nums[mid], nums[low]);
+                    low++;
+                    ++mid;
             }
-            else if(nums[i] == 1){
-                ++b;
-            }
-            else{
-                ++c;
-            }
-        }
-        for(int i= 0; i<n; i++){
-            if(i<a){
-                nums[i] = 0;
-                continue;
-            }
-            else if(i >= a and i< (a+b)){
-                nums[i] = 1;
+            else if(nums[mid] == 1){
+                mid++;
             }
             else{
-                nums[i] = 2;
+                swap(nums[mid], nums[high]);
+                    high--;
             }
         }
-        return ;
+
+        return;
 
     }
 };
