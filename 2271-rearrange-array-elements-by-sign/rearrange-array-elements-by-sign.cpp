867cf89b84157556bbdf2cd4576dad412e5aeccd
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-01-07 18:12:42 +0530

    Time: 161 ms (68.84%) | Memory: 124.5 MB (45.08%) - LeetSync

--- 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
+++ 2271-rearrange-array-elements-by-sign/rearrange-array-elements-by-sign.cpp
@@ -2,35 +2,56 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         // Brute force
-        int size = nums.size();
-        vector < int > positives;
-        vector < int > negatives;
-        vector < int > resultantArray(size);
+        // int size = nums.size();
+        // vector < int > positives;
+        // vector < int > negatives;
+        // vector < int > resultantArray(size);
         
-        for(int index = 0; index < size; index++)
+        // for(int index = 0; index < size; index++)
+        // {
+        //     if(nums[index] < 0)
+        //     {
+        //         negatives.push_back(nums[index]);
+        //     }
+        //     else
+        //     {
+        //         positives.push_back(nums[index]);
+        //     }
+        // }
+        // int negativeIterator = 0;
+        // int positiveIterator = 0;
+        // for(int index = 0; index < size; index++)
+        // {
+        //     if(index % 2 == 0)
+        //     {
+        //         resultantArray[index] = positives[positiveIterator++];
+        //     }
+        //     else
+        //     {
+        //         resultantArray[index] = negatives[negativeIterator++];
+        //     }
+        // }
+        // return resultantArray;
+
+        int size = nums.size();
+        vector < int > resultantArray (size);
+        int positiveIndex = 0;
+        int negativeIndex = 1;
+        for(int index = 0; index < size ; index++)
         {
-            if(nums[index] < 0)
+            if(nums[index] >= 0)
             {
-                negatives.push_back(nums[index]);
+                resultantArray[positiveIndex] = nums[index];
+                positiveIndex+= 2;
             }
             else
             {
-                positives.push_back(nums[index]);
-            }
-        }
-        int negativeIterator = 0;
-        int positiveIterator = 0;
-        for(int index = 0; index < size; index++)
-        {
-            if(index % 2 == 0)
-            {
-                resultantArray[index] = positives[positiveIterator++];
-            }
-            else
-            {
-                resultantArray[index] = negatives[negativeIterator++];
+                resultantArray[negativeIndex] = nums[index];
+                negativeIndex+= 2;
             }
         }
         return resultantArray;
+
+
     }
 };
