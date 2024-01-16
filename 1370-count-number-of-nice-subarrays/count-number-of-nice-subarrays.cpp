6ff90d6a9d523547954ba55f9b5ce912a30fba03
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-01-16 10:46:00 +0530

    Time: 362 ms (5.22%) | Memory: 103 MB (5.29%) - LeetSync

---
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int numberOfSubarrays(vector<int>& nums, int k) {
+        int size = nums.size();
+        for(int i= 0; i < size; i++)
+        {
+            if(nums[i] % 2 == 0)
+            {
+                nums[i] = 0;
+            }
+            else
+            {
+                nums[i] = 1;
+
+            }
+        }
+        for(auto it: nums)
+        {
+            cout << it << " ";
+        }
+         unordered_map <int , int> mpp;
+        int ans= 0;
+        int prefixSum = 0;
+        mpp[0] =1;
+        for(int i= 0; i<size; i++)
+        {   prefixSum+= nums[i];
+            int remaining = prefixSum - k;
+            ans+= (mpp[remaining]);
+            mpp[prefixSum]++;
+        }   
+        return ans;
+        // return 0;
+    }
+};
