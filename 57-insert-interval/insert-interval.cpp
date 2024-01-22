7bb88b817ed62f69f76998f5512c8342633128f7
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-01-22 16:17:28 +0530

    Time: 17 ms (39.65%) | Memory: 20.5 MB (5.25%) - LeetSync

---
+++ 57-insert-interval/insert-interval.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        intervals.push_back(newInterval);
+        sort(intervals.begin(), intervals.end());
+        vector<vector<int>> newTime;
+        // newTime.push_back(intervals[0]);
+        for(int i = 0; i < intervals.size();i++)
+        {
+            if(newTime.empty() == true or newTime.back()[1] < intervals[i][0])
+            {
+                newTime.push_back(intervals[i]);
+            }
+            else
+            {
+                newTime.back()[1] = max(newTime.back()[1], intervals[i][1]);
+            }
+        }
+        return newTime;
+
+    }
+};
