b38339932d6dacf809d9b5e7937d5dafc2743fe9
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-04-25 19:36:13 +0530

    Time: 29 ms (97.90%) | Memory: 11.3 MB (100.00%) - LeetSync

--- 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
+++ 2444-longest-ideal-subsequence/longest-ideal-subsequence.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
     int longestIdealString(string s, int k) {
-        /*
-        apne as pas walo me jis jis se connect ho skte ho unme se jisme max h use 
-        connect ho jao
-        */
-        int arr[26]={0},result=0;
-        int a=0,b=25;
-        for(int i=0;i<s.length();i++){
-            a=0,b=25;
-            int l=s[i]-'a';
-            a=max(a,l-k);
-            b=min(b,l+k);
-            int max=0;
-            //cout<<a<<" "<<b<<" "<<l<<" ";
-            while(a<=b){
-               if(arr[a]>max) max=arr[a];
+        int arr[26] = {0}, result = 0;
+        int a = 0, b = 25;
+        for (int i = 0; i < s.length(); i++) {
+            a = 0, b = 25;
+            int l = s[i] - 'a';
+            a = max(a, l - k);
+            b = min(b, l + k);
+            int max = 0;
+            while (a <= b) {
+                if (arr[a] > max)
+                    max = arr[a];
                 a++;
             }
-           // cout<<max<<endl;
-            arr[l]=max+1;
-            if(arr[l]>result) result=arr[l];
+            arr[l] = max + 1;
+            if (arr[l] > result)
+                result = arr[l];
         }
         return result;
     }
