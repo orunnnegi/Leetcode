392a2c1be05f200f330918ead1a25490cfa089d7
Arun Negi <93508757+orunnnegi@users.noreply.github.com>
2024-07-15 20:28:34 +0530

    Time: 666 ms (80.13%), Space: 276.3 MB (59.39%) - LeetHub

---
+++ 2306-create-binary-tree-from-descriptions/2306-create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, int> inDeg;
+        unordered_map<int, TreeNode*> valueNodeMp;
+
+        for (auto& des : descriptions) {
+            int parent = des[0], child = des[1];
+            bool isLeftChild = (des[2] == 1);
+
+            if (inDeg.find(parent) == inDeg.end()) inDeg[parent] = 0;
+            inDeg[child]--;
+
+            if (valueNodeMp.find(parent) == valueNodeMp.end()) 
+                valueNodeMp[parent] = new TreeNode(parent);
+            if (valueNodeMp.find(child) == valueNodeMp.end()) 
+                valueNodeMp[child] = new TreeNode(child);
+
+            if (isLeftChild) {
+                valueNodeMp[parent]->left = valueNodeMp[child];
+            } else {
+                valueNodeMp[parent]->right = valueNodeMp[child];
+            }
+        }
+
+        for (auto& entry : inDeg) {
+            if (entry.second == 0) 
+                return valueNodeMp[entry.first];
+        }
+
+        return nullptr;
+    }
+};
